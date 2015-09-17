3bf37041e0f5759ea15048b68d823940c3204851
Ruth <ruth@fes-MacBook-Air.local>
2015-09-17 21:18:21 +0800

    Created initial specs for index and new page.
    Signed-off-by: Ruth <ruth@fes-MacBook-Air.local>

--- rails/spec/controllers/questions_controller_spec.rb
+++ rails/spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, :type => :controller do
-  it 'is missing specs'
+  describe "GET #index" do
+  	it "renders the index template" do
+  		get :index
+  		expect(response).to render_template("index")
+  	end
+
+  	it "loads the list of all questions" do
+  		question1 = Question.create(question: "Question 1", answer: "Answer 1")
+      	question2 = Question.create(question: "Question 2", answer: "Answer 2")
+      	questionList = [question1, question2]
+
+  		total = Question.all.count
+  		get :index
+  		expect(assigns(:questions).length).to eq total
+  		expect(assigns(:questions)).to eq(questionList)
+  	end
+  end
+
+  describe "GET #new" do
+  	it "renders the new template" do
+  		get :new
+  		expect(response).to render_template("new")
+  	end
+
+  	it "creates the new question instance" do
+  		get :new
+  		expect(assigns(:question)).to be_instance_of(Question)
+  	end
+  end
+
 end
