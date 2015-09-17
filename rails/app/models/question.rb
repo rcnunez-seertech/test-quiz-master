81f2f4832f7260770750b409e8870ed599ff8e05
Ruth <ruth@fes-MacBook-Air.local>
2015-09-17 20:56:29 +0800

    Added conversion to words and numbers to pass the last test.
    Signed-off-by: Ruth <ruth@fes-MacBook-Air.local>

--- rails/app/models/question.rb
+++ rails/app/models/question.rb
@@ -1,10 +1,17 @@
+require 'numbers_in_words'
+require 'numbers_in_words/duck_punch'
+
 class Question < ActiveRecord::Base
 
   validates_presence_of :question
   validates_presence_of :answer
 
   def is_correct?(submission)
-    answer.squish.downcase == submission.squish.downcase
+    convert_numbers(answer.squish.downcase) == convert_numbers(submission.squish.downcase)
+  end
+
+  def convert_numbers(answer)
+  	answer.match(/^-?\d+$/) ? Integer(answer).in_words : answer
   end
 
 end
